d419e6cf3851473520c27972c5ecfc65bb500a0d
Kenneth Ajah <ajahk@activedgetechnologies.com>
2025-01-03 17:22:43 +0100

    completes the dashboard ui setup

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -6,16 +6,65 @@ import EmailIcon from "@mui/icons-material/Email";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
-import Header from "../../components/Header";
+
 import LineChart from "../../components/LineChart";
 import GeographyChart from "../../components/GeographyChart";
 import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 import Transcale from "../../components/TransitionScale";
+import { TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+import { DataGrid } from "@mui/x-data-grid";
+
+import { mockDataInvoices } from "../../data/mockData";
+import Header from "../../components/Header";
 const Dashboard = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Account Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Type",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Amount",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
 
   return (
     <Box m="20px">
@@ -41,22 +90,10 @@ const Dashboard = () => {
 
       {/* GRID & CHARTS */}
 
-      <Box mb="30px">
-        <h3
-          // variant="h3"
-          // color={colors.grey[100]}
-          // fontWeight="bold"
-          // sx={{ m: "0 0 5px 0" }}
-          className={` italic font-semibold  text-[${colors.grey[100]}] text-xl`}
-        >
-          Overview
-        </h3>
-      </Box>
       <Box
-        display="grid"
         // gridTemplateColumns="repeat(12, 1fr)"
-        gridAutoRows="full"
-        gap="20px"
+        // gridAutoRows="full"
+        gap="30px"
       >
         {/* ROW 1 */}
         {/* <Box
@@ -141,7 +178,7 @@ const Dashboard = () => {
         </Transcale>
 
         {/* ROW 2 */}
-        {/* <Box
+        <Box
           gridColumn="span 8"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -180,7 +217,7 @@ const Dashboard = () => {
           <Box height="250px" m="-20px 0 0 0">
             <LineChart isDashboard={true} />
           </Box>
-        </Box> */}
+        </Box>
         <Box
           gridColumn="span 4"
           gridRow="span 2"
@@ -233,105 +270,262 @@ const Dashboard = () => {
         </Box>
 
         {/* ROW 3 */}
-        {/* <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h5" fontWeight="600">
-            Campaign
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            mt="25px"
-          >
-            <ProgressCircle size="125" />
-            <Typography
-              variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
-            >
-              $48,352 revenue generated
-            </Typography>
-            <Typography>Includes extra misc expenditures and costs</Typography>
-          </Box>
-        </Box> */}
+     
+
+        {/* new section */}
 
         <Box mb="30px">
           <Typography
             variant="h2"
             color={colors.grey[100]}
             fontWeight="bold"
-            sx={{ m: "0 0 5px 0" }}
+            sx={{ mb: 2 }}
           >
             Loan Management
           </Typography>
-          {/* <Typography variant="h5" color={colors.greenAccent[400]}>
-            {subtitle}
-          </Typography> */}
 
-          <div className="grid lg:grid-cols-[60%,40%] gap-5 w-full">
-            <Transcale className=" pb-5 bg-white  rounded-[10px]  pt-2">
-              <Box
-                gridColumn="span 4"
-                gridRow="span 2"
-                backgroundColor={colors.primary[400]}
-                overflow="auto"
+          <Box
+            display="flex"
+            flexDirection={{ xs: "column", md: "row" }}
+            justifyContent="space-between"
+            alignItems="flex-start"
+            gap={3}
+          >
+            {/* Loan History Section */}
+            <Box
+              flex={1}
+              backgroundColor={colors.primary[400]}
+              overflow="auto"
+              borderRadius="8px"
+              boxShadow={3}
+              p={2}
+            >
+              <Typography
+                color={colors.grey[100]}
+                variant="h5"
+                fontWeight="600"
+                mb={2}
+                borderBottom={`4px solid ${colors.primary[500]}`}
+                pb={1}
               >
+                Loan History
+              </Typography>
+              {mockTransactions.slice(0, 5).map((transaction, i) => (
                 <Box
+                  key={`${transaction.txId}-${i}`}
                   display="flex"
                   justifyContent="space-between"
                   alignItems="center"
-                  borderBottom={`4px solid ${colors.primary[500]}`}
-                  colors={colors.grey[100]}
-                  p="15px"
+                  borderBottom={`2px solid ${colors.primary[500]}`}
+                  p={2}
                 >
-                  <Typography
-                    color={colors.grey[100]}
-                    variant="h5"
-                    fontWeight="600"
-                  >
-                    Loan History
-                  </Typography>
-                </Box>
-                {mockTransactions.slice(0, 5).map((transaction, i) => (
-                  <Box
-                    key={`${transaction.txId}-${i}`}
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    borderBottom={`4px solid ${colors.primary[500]}`}
-                    p="15px"
-                  >
-                    <Box>
-                      <Typography
-                        color={colors.greenAccent[500]}
-                        variant="h5"
-                        fontWeight="600"
-                      >
-                        {transaction.txId}
-                      </Typography>
-                      <Typography color={colors.grey[100]}>
-                        {transaction.user}
-                      </Typography>
-                    </Box>
-                    <Box color={colors.grey[100]}>{transaction.date}</Box>
-                    <Box
-                      backgroundColor={colors.greenAccent[500]}
-                      p="5px 10px"
-                      borderRadius="4px"
+                  <Box>
+                    <Typography
+                      color={colors.greenAccent[500]}
+                      variant="h6"
+                      fontWeight="600"
                     >
-                      ${transaction.cost}
-                    </Box>
+                      {transaction.txId}
+                    </Typography>
+                    <Typography color={colors.grey[100]}>
+                      {transaction.user}
+                    </Typography>
                   </Box>
-                ))}
-              </Box>
-            </Transcale>{" "}
-            <Transcale className=" pb-5 bg-white  rounded-[10px]  pt-2"></Transcale>{" "}
-          </div>
+                  <Box color={colors.grey[100]}>{transaction.date}</Box>
+                  <Box
+                    backgroundColor={colors.greenAccent[500]}
+                    p="5px 10px"
+                    borderRadius="4px"
+                    color={colors.grey[100]}
+                  >
+                    ${transaction.cost}
+                  </Box>
+                </Box>
+              ))}
+            </Box>
+
+            {/* Active Loan Details Section */}
+            <Box
+              flex={1}
+              backgroundColor={colors.primary[400]}
+              borderRadius="8px"
+              boxShadow={3}
+              p={2}
+            >
+              <Typography
+                color={colors.grey[100]}
+                variant="h5"
+                fontWeight="600"
+                mb={2}
+                borderBottom={`4px solid ${colors.primary[500]}`}
+                pb={1}
+              >
+                Active Loan Details
+              </Typography>
+              <Typography color={colors.grey[100]}>
+                {/* Render active loan details dynamically */}
+                {true ? (
+                  <>
+                    <strong>Loan Amount:</strong> ${"6,0000"} <br />
+                    <strong>Interest Rate:</strong> {"30"}% <br />
+                    <strong>Tenure:</strong> {"3"} months
+                  </>
+                ) : (
+                  "No active loan details available."
+                )}
+              </Typography>
+
+              <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={initialValues}
+                validationSchema={checkoutSchema}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleBlur,
+                  handleChange,
+                  handleSubmit,
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <Box
+                      display="grid"
+                      gap="30px"
+                      gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                      mt={"20px"}
+                      sx={{
+                        "& > div": {
+                          gridColumn: isNonMobile ? undefined : "span 4",
+                        },
+                      }}
+                    >
+                      {/* <TextField
+                        fullWidth
+                        variant="filled"
+                        type="text"
+                        label="First Name"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.firstName}
+                        name="firstName"
+                        error={!!touched.firstName && !!errors.firstName}
+                        helperText={touched.firstName && errors.firstName}
+                        sx={{ gridColumn: "span 2" }}
+                      />
+                      <TextField
+                        fullWidth
+                        variant="filled"
+                        type="text"
+                        label="Last Name"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.lastName}
+                        name="lastName"
+                        error={!!touched.lastName && !!errors.lastName}
+                        helperText={touched.lastName && errors.lastName}
+                        sx={{ gridColumn: "span 2" }}
+                      /> */}
+                      <TextField
+                        fullWidth
+                        variant="filled"
+                        type="text"
+                        label="Terms"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.email}
+                        name="email"
+                        error={!!touched.email && !!errors.email}
+                        helperText={touched.email && errors.email}
+                        sx={{ gridColumn: "span 4" }}
+                        mt="20px"
+                      />
+                      <TextField
+                        fullWidth
+                        variant="filled"
+                        type="number"
+                        label="Amount"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.contact}
+                        name="contact"
+                        error={!!touched.contact && !!errors.contact}
+                        helperText={touched.contact && errors.contact}
+                        sx={{ gridColumn: "span 4" }}
+                      />
+                      <TextField
+                        fullWidth
+                        variant="filled"
+                        type="text"
+                        label="Purpose of the loan"
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        value={values.address1}
+                        name="address1"
+                        error={!!touched.address1 && !!errors.address1}
+                        helperText={touched.address1 && errors.address1}
+                        sx={{ gridColumn: "span 4" }}
+                        multiline
+                      />
+                    </Box>
+                    <Box display="flex" justifyContent="end" mt="20px">
+                      <Button
+                        type="submit"
+                        color="secondary"
+                        variant="contained"
+                      >
+                        Request a Loan
+                      </Button>
+                    </Box>
+                  </form>
+                )}
+              </Formik>
+            </Box>
+          </Box>
+        </Box>
+
+        {/* Loansection commented out */}
+
+        <Box m="20px">
+          <Header
+            title="TRANSACTION HISTORY"
+            subtitle="List of all transactions history"
+          />
+          <Box
+            m="40px 0 0 0"
+            height="75vh"
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none",
+              },
+              "& .name-column--cell": {
+                color: colors.greenAccent[300],
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: colors.blueAccent[700],
+                borderBottom: "none",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: colors.primary[400],
+              },
+              "& .MuiDataGrid-footerContainer": {
+                borderTop: "none",
+                backgroundColor: colors.blueAccent[700],
+              },
+              "& .MuiCheckbox-root": {
+                color: `${colors.greenAccent[200]} !important`,
+              },
+            }}
+          >
+            <DataGrid
+              checkboxSelection
+              rows={mockDataInvoices}
+              columns={columns}
+            />
+          </Box>
         </Box>
 
         <Box
@@ -370,6 +564,29 @@ const Dashboard = () => {
       </Box>
     </Box>
   );
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
 };
 
 export default Dashboard;
